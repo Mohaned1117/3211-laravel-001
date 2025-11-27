a7d78a93e337f22613f219eb87b87568c655f0ea
Mo'men Hamdy <moamen.hamdy@trianglz.com>
2025-11-27 19:21:57 +0200

    Create Migrations

---
+++ database/migrations/2025_11_27_170656_create_reactions.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->bigInteger('type_id');
+            $table->bigInteger('reaction_type_id');
+            $table->string('type_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reactions');
+    }
+};
